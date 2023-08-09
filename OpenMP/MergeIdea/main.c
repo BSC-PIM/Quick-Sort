1b2648de6abc28647ae2dbba2924bff615d8471c
halfblood <arashyadegari0402@gmail.com>
2023-08-09 15:54:48 +0330

    simple manual test

--- OpenMP/MergeIdea/main.c
+++ OpenMP/MergeIdea/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-#include <omp.h>
-#include <malloc.h>
-#include "utils/work_queue.h"
+#include <stdlib.h>
+#include "utils.h"
+
+#include "quicksort_mt_tp.h"
+
+
+#define CACHE_LINE_SIZE 128
+#define ELEM_N 10
+#define THREAD_NUM 2
+#define PARALLEL 1
 
 
 int main() {
+    int max_threads = omp_get_max_threads();
+    QS_THREAD_NUM = MIN(max_threads, THREAD_NUM);
 
-    work_queue_t *wq = malloc(sizeof(work_queue_t));
-    work_queue_init(wq);
-
-    int num_threads = omp_get_max_threads();
-
-#pragma omp parallel for default(none) firstprivate(num_threads)
-    for (int i = 0; i < num_threads; i++) {
-        printf("THREAD FROM THREAD POOL WITH ID : %d \n", omp_get_thread_num());
-    }
-
+    uint64_t *array = malloc(sizeof(uint64_t) * ELEM_N);
+    POPULATE_ARR(array, ELEM_N);
+    PRINT_ARR(array, ELEM_N);
+    quicksort_multi_thread_thread_pool(array, ELEM_N);
+    PRINT_ARR(array, ELEM_N);
 }
