ebb96256a0627802f1128ab4fc284a2eaf57c57b
halfblood <arashyadegari0402@gmail.com>
2023-08-11 15:44:29 +0330

    refactor the plot color scheme

--- OpenMP/DpuLevel/benchmarks/plotter_scripts/min_chunck_size_plotter.py
+++ OpenMP/DpuLevel/benchmarks/plotter_scripts/min_chunck_size_plotter.py
@@ -15,7 +15,7 @@ for i in range(len(data)):
     y[i] = float(y[i])
 
 fig, ax = plt.subplots()
-ax.plot(x, y, marker='o', linestyle='-', color='b')
+ax.plot(x, y, marker='o', linestyle='--', color='#e76680')  # Change the line color
 ax.set_title('Time vs Minimum Chunk Size (n = 1,000,000)')
 ax.set_ylabel('Time (ms)')
 ax.set_xlabel('Minimum Chunk Size')
@@ -29,10 +29,9 @@ min_y = y[min_y_index]
 # Annotate the minimum point with an arrow and text
 ax.annotate(f'Min: {min_y:.2f} ms at {min_x}',
             xy=(min_x, min_y),
-            xytext=(min_x * 2.2, min_y * 2),  # Adjust the arrow text position
-            arrowprops=dict(facecolor='blue', arrowstyle='->', linewidth=2),  # Make the arrow bolder
-            ha='center', fontsize=12)
+            xytext=(min_x * 2.2, min_y * 2.2),  # Adjust the arrow text position
+            arrowprops=dict(edgecolor='skyblue', arrowstyle='->', linewidth=2),
+            # Change the arrow color and make it bolder
+            ha='center', fontsize=11)
 
 plt.show()
-
-fig.savefig('../results/min_chunk_size.png')
