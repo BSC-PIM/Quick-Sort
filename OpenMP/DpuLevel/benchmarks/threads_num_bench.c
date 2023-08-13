c214f1eb8b4d17772e842a1670f116f6c35685b4
halfblood <arashyadegari0402@gmail.com>
2023-08-13 09:13:29 +0330

    update flag structures based on Flag-Parser lib

--- OpenMP/DpuLevel/benchmarks/threads_num_bench.c
+++ OpenMP/DpuLevel/benchmarks/threads_num_bench.c
@@ -20,18 +20,29 @@ bool verify_wrapper(uint64_t *arr, size_t size) {
 
 int main(int argc, char *argv[]) {
     int threads;
-    int array_size;
+    size_t array_size;
 
     // Define the flags
     struct Flag flags[] = {
-            {"threads",    required_argument, &threads,    NUMBER, THREADS_SIZE,    "Number of threads to use"},
-            {"array_size", required_argument, &array_size, NUMBER, ARRAY_SIZE_FLAG, "Size of the array to sort"},
-            {NULL} // Terminating entry
+            {.name = "threads", .has_arg=required_argument, .val='t', .save=&threads, .type = INT16_T},
+            {.name = "array-size", .has_arg=required_argument, .val='a', .save=&array_size, .type= INT64_T}
     };
 
 
+    const char *Usage = "Usage: program_name [OPTIONS]\n\n"
+                        "Description:\n"
+                        "  This program demonstrates flag parsing using the flag_parser library.\n\n"
+                        "Options:\n"
+                        "--threads NUM                         Number of threads to use (required, range: 0 to 32,767)\n"
+                        "--array-size NUM                      Size of the array (required, range: 0 to 32,767)\n"
+                        "--help                                Display this help message and exit\n\n"
+                        "Usage Examples:\n"
+                        "  program_name --threads 4 --array-size 100   # Run with 4 threads and array size 100\n";
+
+
     // Parse the flags
-    parse_flags(argc, argv, flags, sizeof(flags) / sizeof(flags[0]));
+    parse_flags(argc, argv, flags, sizeof(flags) / sizeof(flags[0]), Usage);
+
 
     uint64_t array[array_size];
 
@@ -49,3 +60,5 @@ int main(int argc, char *argv[]) {
     bench_function(quicksort_threadpool_parallelism, array, array_size, TEST_COUNT, HOARE, setup, verify);
     printf("\n");
 }
+
+//TODO CHECK STD IMPLEMENTATION
