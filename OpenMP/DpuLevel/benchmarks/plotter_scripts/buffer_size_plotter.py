3909d7525615e3daab27e2d1850f538155c0843e
halfblood <arashyadegari0402@gmail.com>
2023-08-17 19:04:57 +0330

    update the plotter

--- OpenMP/DpuLevel/benchmarks/plotter_scripts/buffer_size_plotter.py
+++ OpenMP/DpuLevel/benchmarks/plotter_scripts/buffer_size_plotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open('../results/buffer_size.csv', 'r') as f:
     data = f.read()
@@ -19,6 +20,24 @@ ax.set_title('Time vs Buffer Size')
 ax.set_ylabel('Time (ms)')
 ax.set_xlabel('Buffer Size')
 
+# Perform linear regression
+coefficients = np.polyfit(x, y, 1)
+poly = np.poly1d(coefficients)
+x_fit = np.linspace(min(x), max(x), 100)
+y_fit = poly(x_fit)
+ax.plot(x_fit, y_fit, color='skyblue', label='Linear Fit')
+
+slope, intercept = coefficients
+slope_sci = "{:.2e}".format(slope)
+intercept_sci = "{:.2e}".format(intercept)
+
+ax.text(0.95, 0.05, f"Slope: {slope_sci}\nIntercept: {intercept_sci}", transform=ax.transAxes, fontsize=12, va='bottom',
+        ha='right')
+
+plt.legend()
 plt.show()
 
 fig.savefig('../results/min_chunk_size.png')
+
+print("Slope:", slope_sci)
+print("Intercept:", intercept_sci)
