04b8c5f2027f43a349904833ac7b49d97abf1fbe
halfblood <arashyadegari0402@gmail.com>
2023-08-21 16:13:07 +0330

    add documentaton and flag parser

--- OpenMP/hierarchicalSort/benchmarks/min_chunk_size_bench.c
+++ OpenMP/hierarchicalSort/benchmarks/min_chunk_size_bench.c
@@ -6,36 +6,73 @@
 #include "common.h"
 #include "flag_parser.h"
 
-#define ARRAY_SIZE_FLAG 1000000
 #define TEST_COUNT 50
-#define GROUP_MIN_DIST_FLAG 1
 
+/**
+ * @brief Populate an array with a specified value.
+ *
+ * This function populates an array with a given value up to the specified size.
+ * The array is populated with the value specified by the maximum value of the data type `uint64_t`.
+ *
+ * @param arr Pointer to the array to be populated.
+ * @param size Size of the array.
+ *
+ * @note The array should have enough memory allocated to accommodate the specified size.
+ * @warning Using this function with large sizes might lead to performance issues.
+ */
 void populate_wrapper(uint64_t *arr, size_t size) {
     POPULATE_ARR(arr, size, INTMAX_MAX);
 }
 
+
+/**
+ * @brief Verify wrapper function.
+ *
+ * This function wraps the `verify` function to provide a consistent interface for verification.
+ *
+ * @param arr Pointer to the array to be verified.
+ * @param size Size of the array.
+ * @return `true` if the array is verified successfully, `false` otherwise.
+ */
 bool verify_wrapper(uint64_t *arr, size_t size) {
     return verify(arr, size);
 }
 
 int main(int argc, char *argv[]) {
-    int array_size, group_min_dist;
-    array_size = ARRAY_SIZE_FLAG;
-    group_min_dist = GROUP_MIN_DIST_FLAG;
+    size_t array_size = 1000000;
+    int threads = 6, group_min_dist = 128;
+
+    // Define the compile flags
+    struct Flag flags[] = {
+            {.name = "array-size", .has_arg=required_argument, .val='a', .save=&array_size, .type= INT64_T},
+            {.name = "threads", .has_arg=required_argument, .val='t', .save=&threads, .type = INT16_T},
+            {.name = "group-min-dist", .has_arg=required_argument, .val='g', .save=&group_min_dist, .type=INT16_T}
+    };
+
+
+    // Usage
+    const char *Usage = "Usage: program_name [OPTIONS]\n\n"
+                        "Description:\n"
+                        "  This program demonstrates effects of switching condition between sequential and parallel in sorting.\n\n"
+                        "Options:\n"
+                        "--threads NUM                         Number of threads to use\n"
+                        "--array-size NUM                      Size of the array\n"
+                        "--group-min-dist NUM                  Minimum size required for a partition to run in parallel\n"
+                        "Usage Examples:\n"
+                        "  program_name --threads 4 --array-size 100  --group-min-dist 128 # Run with 4 threads and array size 100 and partition-min-dist 128\n";
+
+    parse_flags(argc, argv, flags, sizeof(flags) / sizeof(flags[0]), Usage);
     uint64_t array[array_size];
 
     // set up the threads
-    omp_set_num_threads(omp_get_num_procs());
+    omp_set_num_threads(threads);
     GROUP_MIN_DIST = group_min_dist;
+
 
     // create a pointer function which is used for setup (get array pointer and array size then call POPULATE_ARR on it)
     void (*setup)(uint64_t *, size_t) = &populate_wrapper;
     bool (*verify)(uint64_t *, size_t) = &verify_wrapper;
 
-    printf("\n");
-    for (int i = 0; i < 12; i++) {
-        GROUP_MIN_DIST = 1 << i;
-        bench_function(quicksort_task_parallelism, array, array_size, TEST_COUNT, HOARE, setup, verify);
-    }
+    bench_function(quicksort_task_parallelism, array, array_size, TEST_COUNT, HOARE, setup, verify);
 
 }
--- OpenMP/hierarchicalSort/benchmarks/threads_num_bench.c
+++ OpenMP/hierarchicalSort/benchmarks/threads_num_bench.c
@@ -10,38 +10,68 @@
 #define ARRAY_SIZE_FLAG 10000
 #define TEST_COUNT 100
 
+
+/**
+ * @brief Populate an array with a specified value.
+ *
+ * This function populates an array with a given value up to the specified size.
+ * The array is populated with the value specified by the maximum value of the data type `uint64_t`.
+ *
+ * @param arr Pointer to the array to be populated.
+ * @param size Size of the array.
+ *
+ * @note The array should have enough memory allocated to accommodate the specified size.
+ * @warning Using this function with large sizes might lead to performance issues.
+ */
 void populate_wrapper(uint64_t *arr, size_t size) {
     POPULATE_ARR(arr, size, INTMAX_MAX);
 }
 
+
+/**
+ * @brief Verify wrapper function.
+ *
+ * This function wraps the `verify` function to provide a consistent interface for verification.
+ *
+ * @param arr Pointer to the array to be verified.
+ * @param size Size of the array.
+ * @return `true` if the array is verified successfully, `false` otherwise.
+ */
 bool verify_wrapper(uint64_t *arr, size_t size) {
     return verify(arr, size);
 }
 
 int main(int argc, char *argv[]) {
-    int threads;
-    size_t array_size;
+    int threads = 6;
+    size_t array_size = 1000000;
 
     // Define the flags
     struct Flag flags[] = {
             {.name = "threads", .has_arg=required_argument, .val='t', .save=&threads, .type = INT16_T},
-            {.name = "array-size", .has_arg=required_argument, .val='a', .save=&array_size, .type= INT64_T}
+            {.name = "array-size", .has_arg=required_argument, .val='a', .save=&array_size, .type= INT64_T},
     };
 
 
     const char *Usage = "Usage: program_name [OPTIONS]\n\n"
                         "Description:\n"
-                        "  This program demonstrates flag parsing using the flag_parser library.\n\n"
+                        "  This program demonstrates different threads number and method effects on sorting.\n\n"
                         "Options:\n"
-                        "--threads NUM                         Number of threads to use (required, range: 0 to 32,767)\n"
-                        "--array-size NUM                      Size of the array (required, range: 0 to 32,767)\n"
-                        "--help                                Display this help message and exit\n\n"
+                        "--threads NUM                         Number of threads to use\n"
+                        "--array-size NUM                      Size of the array\n"
                         "Usage Examples:\n"
                         "  program_name --threads 4 --array-size 100   # Run with 4 threads and array size 100\n";
 
 
     // Parse the flags
     parse_flags(argc, argv, flags, sizeof(flags) / sizeof(flags[0]), Usage);
+
+    // TODO implement optional arg
+    if (!flags[0].present) {
+        printf("use default setup for `thread` flag. threads := %d\n", threads);
+    }
+    if (!flags[1].present) {
+        printf("use default setup for `array-size` flag. array-size := %zu\n", array_size);
+    }
 
 
     uint64_t array[array_size];
@@ -54,11 +84,12 @@ int main(int argc, char *argv[]) {
     void (*setup)(uint64_t *, size_t) = &populate_wrapper;
     bool (*verify)(uint64_t *, size_t) = &verify_wrapper;
 
-    // benchmark the quicksort_task_parallelism
+    // benchmark the quicksort baseline model (sequential)
     bench_function(quicksort_baseline, array, array_size, TEST_COUNT, HOARE, setup, verify);
-    bench_function(quicksort_task_parallelism, array, array_size, TEST_COUNT, HOARE, setup, verify);
-    bench_function(quicksort_threadpool_parallelism, array, array_size, TEST_COUNT, HOARE, setup, verify);
-    printf("\n");
-}
 
-//TODO CHECK STD IMPLEMENTATION
+    // benchmark the quicksort task parallelism model
+    bench_function(quicksort_task_parallelism, array, array_size, TEST_COUNT, HOARE, setup, verify);
+
+    // benchmark the quicksort threadpool model
+    bench_function(quicksort_threadpool_parallelism, array, array_size, TEST_COUNT, HOARE, setup, verify);
+}
