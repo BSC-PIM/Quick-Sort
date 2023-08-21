e44fa2619b95c2bb8c310d76fc6125c590f64ea7
halfblood <arashyadegari0402@gmail.com>
2023-08-21 16:13:39 +0330

    add document

--- OpenMP/hierarchicalSort/benchmarks/commons/common.c
+++ OpenMP/hierarchicalSort/benchmarks/commons/common.c
@@ -1,9 +1,18 @@
-//
-// Created by halfblood on 8/11/23.
-//
-
 #include "common.h"
 
+/**
+ * @brief Verify if an array is sorted in ascending order.
+ *
+ * This function checks if the elements of the array are in ascending order up to the specified size.
+ * If any element is found to be greater than the subsequent element, an error message is printed, and
+ * the function returns `false`, indicating that the array is not sorted.
+ *
+ * @param arr Pointer to the array to be verified.
+ * @param size Size of the array.
+ * @return `true` if the array is sorted in ascending order, `false` otherwise.
+ *
+ * @note The array should have at least `size` elements.
+ */
 bool verify(uint64_t *arr, size_t size) {
     for (int i = 0; i < size - 1; i++) {
         if (arr[i] > arr[i + 1]) {
@@ -14,6 +23,27 @@ bool verify(uint64_t *arr, size_t size) {
     return true;
 }
 
+
+/**
+ * @brief Benchmark a function's performance.
+ *
+ * This function benchmarks the performance of a given function that operates on an array.
+ * The function is benchmarked for a specified number of times (`test_count`).
+ * A setup function is called before each benchmark to prepare the array.
+ * After calling the benchmarked function, a verification function is called to verify the results.
+ * The average execution time across all tests is calculated and printed in milliseconds.
+ *
+ * @param func The function to be benchmarked.
+ * @param arr Pointer to the array used for benchmarking.
+ * @param size Size of the array.
+ * @param test_count Number of times to benchmark the function.
+ * @param method The partition method used by the benchmarked function.
+ * @param setup The setup function to prepare the array before each benchmark.
+ * @param verify The verification function to validate the results after each benchmark.
+ *
+ * @note The array should have at least `size` elements.
+ * @warning Make sure the setup and verification functions work correctly with the benchmarked function.
+ */
 void bench_function(bench_func func, uint64_t *arr, size_t size, size_t test_count, partition_method_t method,
                     setup_func setup, verify_func verify) {
     double start, end, avg = 0;
@@ -35,6 +65,16 @@ void bench_function(bench_func func, uint64_t *arr, size_t size, size_t test_cou
 }
 
 
+/**
+ * @brief Baseline quicksort function for comparison.
+ *
+ * This function implements a baseline version of the quicksort algorithm.
+ * It operates on an array of `uint64_t` elements and sorts them in ascending order.
+ *
+ * @param arr Pointer to the array to be sorted.
+ * @param size Size of the array.
+ * @param method The partition method used by the quicksort algorithm.
+ */
 void quicksort_baseline(uint64_t *arr, size_t size, partition_method_t method) {
     quicksort_rec_seq(arr, 0, size - 1, method);
 }
