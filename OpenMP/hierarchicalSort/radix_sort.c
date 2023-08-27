f7cf31464072e3f1af83e91dbf748fdb889fb59b
halfblood <arashyadegari0402@gmail.com>
2023-08-27 08:56:31 +0330

    remove extra-files

--- OpenMP/hierarchicalSort/radix_sort.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by halfblood on 8/18/23.
-//
-
-#include "radix_sort.h"
--- OpenMP/hierarchicalSort/radix_sort.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by halfblood on 8/18/23.
-//
-
-#ifndef DPULEVEL_RADIX_SORT_H
-#define DPULEVEL_RADIX_SORT_H
-
-#endif //DPULEVEL_RADIX_SORT_H
